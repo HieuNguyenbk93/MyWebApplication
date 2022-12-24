b63be57da926235b7e86793537a2f3876f53fe8f
Hieu Nguyen <hieunguyenbk93@gmail.com>
2022-12-24 10:59:25 +0700

    Add model, call Backend, pass data from js to view

--- MyWebApplication/Controllers/QuestionController.cs
+++ MyWebApplication/Controllers/QuestionController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MyWebApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +14,24 @@ namespace MyWebApplication.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetListTest()
+        {
+            try
+            {
+                IList<Student> studentList = new List<Student>() {
+                    new Student() { StudentID = 1, StudentName = "John" },
+                    new Student() { StudentID = 2, StudentName = "Moin" },
+                    new Student() { StudentID = 3, StudentName = "Bill" },
+                    new Student() { StudentID = 4, StudentName = "Ram" },
+                    new Student() { StudentID = 5, StudentName = "Ron"  }
+                };
+                return Json(studentList, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, ex.Message);
+            }
         }
     }
 }
--- MyWebApplication/ScriptControllers/QuestionJs.js
+++ MyWebApplication/ScriptControllers/QuestionJs.js
@@ -1,3 +1,26 @@
 ﻿myApp.controller("QuestionJs", function ($scope, $http) {
     console.log("Nhấn link question, bật F12 show log");
+
+    $scope.listData = [];
+
+    var getListTest = function () {
+        return $http({
+            url: "/Question/GetListTest",
+            method: "GET",
+        });
+    }
+
+    var init = function () {
+        getListTest().then(function successCallback (response) {
+            console.log(response);
+            if (response.status == 200) {
+                $scope.listData = response.data;
+                console.log($scope.listData);
+            }
+        }, function errorCallback(error) {
+            console.log(error);
+        });
+    }
+
+    init();
 });
